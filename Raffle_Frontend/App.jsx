fbda1a81c6abd0b4425aa49bf3e0e60f77e0c532
cryptodiscolab <cryptodiscolab@gmail.com>
2026-01-31 19:27:35 +0700

    Fix import paths in App.jsx

--- Raffle_Frontend/App.jsx
+++ Raffle_Frontend/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { Web3Provider } from './providers/Web3Provider';
-import { Header } from './components/Header';
-import { HomePage } from './pages/HomePage';
-import { RafflesPage } from './pages/RafflesPage';
-import { LeaderboardPage } from './pages/LeaderboardPage';
-import { ProfilePage } from './pages/ProfilePage';
-import { CreateRafflePage } from './pages/CreateRafflePage';
+import { Web3Provider } from './Web3Provider';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+import { RafflesPage } from './RafflesPage';
+import { LeaderboardPage } from './LeaderboardPage';
+import { ProfilePage } from './ProfilePage';
+import { CreateRafflePage } from './CreateRafflePage';
 
 function App() {
   return (
